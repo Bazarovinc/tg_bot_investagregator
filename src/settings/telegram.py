2ef123e82e12a87b0018c51715e547c756686033
Nikita Veselenko <n.veselenko@idaproject.com>
2025-10-11 15:08:48 +0300

    fix

--- src/settings/telegram.py
+++ src/settings/telegram.py
@@ -5,4 +5,4 @@ from pydantic_settings import BaseSettings
 class TelegramSettings(BaseSettings):
     admin_chat_id: int
     token: SecretStr
-    webhook: AnyHttpUrl = "https://bbae6hvom2ckgp3f1fob.containers.yandexcloud.net"
+    webhook: AnyHttpUrl
